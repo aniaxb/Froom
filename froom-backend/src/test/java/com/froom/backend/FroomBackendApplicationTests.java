e7975dfe010f6d23da7b42e4606309e5765ad0ca
Ania Banachowicz <101459177+aniaxb@users.noreply.github.com>
2023-09-25 18:58:02 +0200

    Create froom-backend

---
+++ froom-backend/src/test/java/com/froom/backend/FroomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.froom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FroomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
