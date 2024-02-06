36b0a1c81ec264ca2bfb2184fb96af25ca296330
Ania Banachowicz <101459177+aniaxb@users.noreply.github.com>
2024-02-07 00:48:36 +0100

    Continued adding general function declarations

--- froom-backend/src/main/kotlin/com/froom/froombackend/items/controller/CategoryController.kt
+++ froom-backend/src/main/kotlin/com/froom/froombackend/items/controller/CategoryController.kt
@@ -1,4 +1,21 @@
 package com.froom.froombackend.items.controller
 
-class CategoryController {
+import com.froom.froombackend.items.service.CategoryService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/category")
+class CategoryController (val categoryService: CategoryService){
+
+    @GetMapping()
+    fun getCategories() {
+
+    }
+
+    @GetMapping("/bodypart")
+    fun getBodyPartCategories() {
+
+    }
 }
--- froom-backend/src/main/kotlin/com/froom/froombackend/items/controller/OutfitController.kt
+++ froom-backend/src/main/kotlin/com/froom/froombackend/items/controller/OutfitController.kt
@@ -1,4 +1,62 @@
 package com.froom.froombackend.items.controller
 
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/outfit")
 class OutfitController {
+
+    @PostMapping()
+    fun createOutfit() {
+    }
+
+    @PutMapping("/{outfitUuid}/item/{itemUuid}")
+    fun addItemToOutfit(
+        @PathVariable outfitUuid: String,
+        @PathVariable itemUuid: String
+    ) {
+    }
+
+    @DeleteMapping("/{outfitUuid}/item/{itemUuid}")
+    fun removeItemFromOutfit(
+        @PathVariable outfitUuid: String,
+        @PathVariable itemUuid: String
+    ) {
+    }
+
+    @GetMapping("/{outfitUuid}")
+    fun getOutfitByUuid(
+        @PathVariable outfitUuid: String
+    ) {
+    }
+
+    @GetMapping()
+    fun getOutfits() {
+    }
+
+    @PutMapping("/{outfitUuid}")
+    fun updateOutfit(
+        @PathVariable outfitUuid: String
+    ) {
+    }
+
+    @DeleteMapping("/{outfitUuid}")
+    fun deleteOutfit(
+        @PathVariable outfitUuid: String
+    ) {
+    }
+
+    @GetMapping("/filter")
+    fun getOutfitsByFilter(
+        @RequestParam(required = false) name: String?,
+    ) {
+    }
+
+    @GetMapping("/random")
+    fun getRandomOutfit() {
+    }
+
+    @PostMapping("/random")
+    fun createRandomOutfit() {
+    }
 }
